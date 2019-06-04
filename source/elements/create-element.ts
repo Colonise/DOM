e6674d6fba4e4eee22c40f87e3d659f71623df2b
Pathurs <pathurs123@gmail.com>
2019-06-04 22:30:11 +1000

    feat(createElement): remove deprecated function

--- source/elements/create-element.ts
+++ source/elements/create-element.ts
@@ -10,12 +10,10 @@ export function createElement<TTagName extends keyof HTMLElementTagNameMap>(
     options?: ElementCreationOptions
 ): HTMLElementTagNameMap[TTagName];
 /**
- * @deprecated
+ * Creates an instance of the element for the specified tag.
+ *
+ * @param tagName The name of an element.
  */
-export function createElement<TTagName extends keyof HTMLElementDeprecatedTagNameMap>(
-    tagName: TTagName,
-    options?: ElementCreationOptions
-): HTMLElementDeprecatedTagNameMap[TTagName];
 export function createElement(tagName: string, options?: ElementCreationOptions): HTMLElement;
 export function createElement(tagName: string, options?: ElementCreationOptions): HTMLElement {
     const element = constants.document.createElement(tagName, options);
